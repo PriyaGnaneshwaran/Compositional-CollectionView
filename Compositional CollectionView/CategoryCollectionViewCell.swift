071028aa46f39dd9ec14b41832f13ec278b7d213
PriyaGnaneshwaran <priyayogi084@gmail.com>
2025-05-18 22:54:19 +0530

    compositional Layout design

---
+++ Compositional CollectionView/CategoryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Compositional CollectionView
+//
+//  Created by Priya Gnaneshwaran on 18/05/25.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
